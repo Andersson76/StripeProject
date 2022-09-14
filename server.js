a92053009e34c532922750ee846de6617074b3d9
Martin Andersson <martin.andersson@me.com>
2022-09-14 19:58:09 +0200

    createCustomer

--- server.js
+++ server.js
@@ -20,11 +20,39 @@ const calculateOrderAmount = (items) => {
   return 1400;
 };
 
+const chargeCustomer = async (customerId) => {
+  // Lookup the payment methods available for the customer
+  const paymentMethods = await stripe.paymentMethods.list({
+    customer: customerId,
+    type: "card",
+  });
+  try {
+    // Charge the customer and payment method immediately
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: "usd",
+      customer: customerId,
+      payment_method: paymentMethods.data[0].id,
+      off_session: true,
+      confirm: true,
+    });
+  } catch (err) {
+    // Error code will be authentication_required if authentication is needed
+    console.log("Error code is: ", err.code);
+    const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(err.raw.payment_intent.id);
+    console.log("PI retrieved: ", paymentIntentRetrieved.id);
+  }
+};
+
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
+  const customer = await stripe.customers.create();
+
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
+    customer: customer.id,
+    setup_future_usage: "off_session",
     amount: calculateOrderAmount(items),
     currency: "sek",
     automatic_payment_methods: {
